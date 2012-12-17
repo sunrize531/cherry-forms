9271a08ffc908796942596663e9f0ae3d051cc95
sunrize <sunrize@wysegames.com>
2012-12-17 21:31:25 +0200

    License and author changed.

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from distutils.core import setup
 setup(
     name='cherry-forms',
     version='0.1a',
-    packages=['tornado', 'cherryforms'],
+    packages=['cherryforms'],
     url='',
     license='MIT',
-    author='sunrize',
-    author_email='sunrize531@gmail.com',
+    author='WYSEGames',
+    author_email='info@wysegames.com',
     description='Set of UIModules for building forms with Tornado web server. Widgets also can be used as standalone on clientside.',
     install_requires=[
         "tornado >= 2.4",
+        "pymongo >= 2.3"
     ],
 )
