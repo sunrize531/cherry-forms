8793aa6c8ad94173a805b8f63712bd5b2e390fa9
sunrize <sunrize@wysegames.com>
2012-12-19 00:50:57 +0200

    Tweaks in setup script.

--- setup.py
+++ setup.py
@@ -3,15 +3,16 @@ from distutils.core import setup
 setup(
     name='cherry-forms',
     version='0.1a',
-    packages=['cherryforms'],
+    packages=['cherryforms',],
     url='',
     license='MIT',
     author='WYSEGames',
     author_email='info@wysegames.com',
-    description='Set of UIModules for building forms with Tornado web server. Widgets also can be used as standalone on clientside.',
+    description=('Set of UIModules for building forms with Tornado web server. '
+                 'Widgets also can be used as standalone on clientside.'),
     install_requires=[
         "tornado >= 2.4",
         "pymongo >= 2.3",
-        "cherrycommon"
+        "cherry-common"
     ],
 )
