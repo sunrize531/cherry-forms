fa820ed28928c9fd7772bea3eb56cb1e7c2b959e
Ivan Filimonov <i.filimonov@wysegames.com>
2013-02-08 21:33:09 +0200

    Version updated

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def get_file_list(path):
 
 setup(
     name='cherry-forms',
-    version='0.2.1a',
+    version='0.2.2a',
     packages=['cherryforms',],
     package_data={'cherryforms': get_file_list('templates') + get_file_list('static')},
     url='',
