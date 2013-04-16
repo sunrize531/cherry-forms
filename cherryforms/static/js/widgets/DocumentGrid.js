491944083ce24b93057752bae0672cb24b640ce2
Ivan Filimonov <i.filimonov@wysegames.com>
2013-04-16 18:13:24 +0300

    Controls now (can) work in DocumentGrid

--- cherryforms/static/js/widgets/DocumentGrid.js
+++ cherryforms/static/js/widgets/DocumentGrid.js
@@ -1,5 +1,6 @@
-define(['underscore', 'backbone', 'handsontable', 'core',
-    'less!chf-document-grid.less', 'css!jquery.handsontable.css'], function (_, Backbone, Handsontable, CherryForms) {
+define(['underscore', 'backbone', 'handsontable', 'core', 'utils', 'widgets/Identifier',
+    'less!chf-document-grid.less', 'css!jquery.handsontable.css'],
+    function (_, Backbone, Handsontable, CherryForms, Utils) {
     "use strict";
 
     var Widgets = CherryForms.Widgets,
@@ -26,7 +27,7 @@ define(['underscore', 'backbone', 'handsontable', 'core',
             }
         }),
 
-        DocumentGrid = Backbone.Collection.extend({
+        DocumentGrid = Models.DocumentsCollection.extend({
             model: Row
         }),
 
@@ -39,7 +40,9 @@ define(['underscore', 'backbone', 'handsontable', 'core',
                     'button_cell': 'chf-cell-button',
                     'copy_button': 'chf-copy-button',
                     'del_button': 'chf-trash-button',
-                    'copy_dialog': 'chf-copy-dialog'
+                    'copy_dialog': 'chf-copy-dialog',
+                    'copy_document_button': 'chf-copy-document-button',
+                    'cancel_document_button': 'chf-cancel-document-button'
                 });
             },
 
@@ -49,6 +52,7 @@ define(['underscore', 'backbone', 'handsontable', 'core',
 
             processValue: function () {
                 this.value = new DocumentGrid(this.get('value'));
+                this.value.url = this.get('crud_url');
                 this.trigger(Events.FIELD_CHANGE, this);
             },
 
@@ -65,10 +69,59 @@ define(['underscore', 'backbone', 'handsontable', 'core',
             },
 
             gridData: function () {
+                var data = [],
+                    rowData;
                 if (!_.isUndefined(this.value)) {
-                    return this.value.toJSON();
+                    return this.value.map(function (row) {
+                        rowData = {};
+                        _.each(row.toJSON(), function (value, key) {
+                            if (!Utils.isSimple(value)) {
+                                rowData[key] = JSON.stringify(value);
+                            } else {
+                                rowData[key] = value;
+                            }
+                        });
+                        return rowData;
+                    });
                 }
                 return [];
+            }
+        }),
+
+        Identifier = Widgets.Identifier,
+
+        COPY_DOCUMENT_CLICK = 'click:copy_document_button',
+        CANCEL_DOCUMENT_CLICK = 'click:cancel_document_button',
+
+        CopyDocumentDialog = Identifier.extend({
+            template: _.template(
+                '<div class="{{ copy_dialog }}">' +
+                    '<div class="modal-header">Copy document</div>' +
+                    '<div class="modal-body">' +
+                        '<label>Save document with new id:</label>' +
+                        '<input type="text" id="{{ input_id }}" value="{{ value }}" class="{{ input_class }}">' +
+                    '</div>' +
+                    '<div class="modal-footer">' +
+                        '<a href="#" class="btn {{ cancel_document_button }}">Cancel</a>' +
+                        '<a href="#" class="btn btn-primary {{ copy_document_button }}">Copy</a>' +
+                    '</div>' +
+                '</div>'
+            ),
+            className: 'modal',
+
+            events: function () {
+                var events = Identifier.prototype.events.apply(this, arguments);
+                events['click .modal-footer .' + this.model.get('copy_document_button')] = 'copyClickHandler';
+                events['click .modal-footer .' + this.model.get('cancel_document_button')] = 'cancelClickHandler';
+                return events;
+            },
+
+            copyClickHandler: function () {
+                this.trigger(COPY_DOCUMENT_CLICK, this.model.value);
+            },
+
+            cancelClickHandler: function () {
+                this.trigger(CANCEL_DOCUMENT_CLICK, this.model.value);
             }
         }),
 
@@ -80,37 +133,19 @@ define(['underscore', 'backbone', 'handsontable', 'core',
                     '<div class="{{ grid_class }}"></div>' +
                 '</div>'),
             idTemplate: _.template('<a href="{{ document_id }}">{{ document_id }}</a>'),
-            documentCopyDialogTemplate: _.template('<div class="{{ copy_dialog }} modal hide fade">' +
-                    '<div class="modal-header">Copy document</div>' +
-                    '<div class="modal-body">' +
-                        '<label>Save document with new id:</label>' +
-                        '<input type="text" value="{{ document_id }}-copy">' +
-                    '</div>' +
-                    '<div class="modal-footer">' +
-                        '<a href="#" class="btn">Cancel</a>' +
-                        '<a href="#" class="btn btn-primary">Copy</a>' +
-                    '</div>' +
-                '</div>'),
 
             initialize: function () {
                 Widget.prototype.initialize.apply(this, arguments);
-                _.bindAll(this, '_documentIDRenderer', '_controlsRenderer', '_controlsClickHandler');
+                _.bindAll(this, 'documentIDRenderer', 'controlsRenderer', 'controlsClickHandler');
             },
 
-            _getDocumentAtRow: function (row) {
-                if (row !== this._currentRow) {
-                    this._currentRow = row;
-                    this._currentDocument = this.model.value.at(row).toJSON();
-                }
-                return this._currentDocument;
-            },
-
-            _documentIDRenderer: function (instance, td, row, col, prop, value, cellProperties) {
+            documentIDRenderer: function (instance, td, row, col, prop, value, cellProperties) {
                 Handsontable.TextRenderer.apply(this, arguments);
                 $(td).html(this.idTemplate({document_id: value}));
             },
 
-            _documentIDEditor: function (instance, td, row, col, prop, keyboardProxy, cellProperties) {
+            documentIDEditor: function (instance, td, row, col, prop, keyboardProxy, cellProperties) {
+                console.log(arguments);
                 keyboardProxy.on("keydown.editor", function (event) {
                     switch (event.keyCode) {
                     case 13:
@@ -121,10 +156,10 @@ define(['underscore', 'backbone', 'handsontable', 'core',
                 });
             },
 
-            _controlsRenderer: function (instance, td, row, col, prop, value, cellProperties) {
+            controlsRenderer: function (instance, td, row, col, prop, value, cellProperties) {
                 var $controls = $(this._controlsTemplate(
                     _.extend({document_id: value}, this.model.toJSON())
-                )).click(this._controlsClickHandler);
+                )).click(this.controlsClickHandler);
                 $(td).html($controls).addClass(this.model.get('controls_class'));
             },
 
@@ -138,12 +173,12 @@ define(['underscore', 'backbone', 'handsontable', 'core',
 
             gridColumns: function (controlsEnabled) {
                 var columns = [{data: '_id', type: {
-                    renderer: this._documentIDRenderer,
-                    editor: this._documentIDEditor
+                    renderer: this.documentIDRenderer,
+                    editor: this.documentIDEditor
                 }}];
 
                 if (controlsEnabled) {
-                    columns.push({data: '_control', type: {renderer: this._controlsRenderer, editor: bullocks}});
+                    columns.push({data: '_control', type: {renderer: this.controlsRenderer, editor: bullocks}});
                 }
                 return columns.concat(_.map(this.model.get('fields'), function (field) {
                     return {data: field};
@@ -152,7 +187,7 @@ define(['underscore', 'backbone', 'handsontable', 'core',
 
             render: function () {
                 Widget.prototype.render.call(this);
-                this._renderGrid();
+                this.renderGrid();
                 return this;
             },
 
@@ -162,7 +197,7 @@ define(['underscore', 'backbone', 'handsontable', 'core',
                 '<i class="icon-trash"></i></a>',
             controlsTemplate: '<div class="">{{ controls }}</div>',
 
-            _renderGrid: function () {
+            renderGrid: function () {
                 var canCopy = this.model.get('can_copy'),
                     canDelete = this.model.get('can_delete'),
                     controlsEnabled = this._controlsEnabled = canCopy || canDelete,
@@ -193,22 +228,71 @@ define(['underscore', 'backbone', 'handsontable', 'core',
                 return this;
             },
 
-            _controlsClickHandler: function (event) {
+            refreshGrid: function () {
+                this.$grid.handsontable('loadData', this.model.gridData());
+                this.$grid.handsontable('render');
+            },
+
+            getCopyDocumentDialog: function () {
+                if (_.isUndefined(this._copyDialog)) {
+                    var copyDialog = this._copyDialog = new CopyDocumentDialog(_.extend(
+                        {},
+                        _.pick(this.model.toJSON(), 'copy_dialog',
+                            'copy_document_button', 'cancel_document_button')
+                    ));
+                    this._copyDialog.render();
+                    this.listenTo(copyDialog, COPY_DOCUMENT_CLICK, this.copyDocumentHandler);
+                    this.listenTo(copyDialog, CANCEL_DOCUMENT_CLICK, this.cancelDocumentHandler);
+                    this.$el.append(copyDialog.$el);
+                }
+                return this._copyDialog;
+            },
+
+            controlsClickHandler: function (event) {
                 var $button = $(event.target).parent('a'),
                     documentID = $button.prop('rel'),
                     crudURL = this.model.get('crud_url'),
-                    $modal;
+                    copyDialog, document;
                 if (crudURL) {
                     if ($button.hasClass(this.model.get('copy_button'))) {
-                        $modal = $(this.documentCopyDialogTemplate(
-                            _.extend({document_id: documentID}, this.model.toJSON())
-                        )).appendTo(this.el).modal();
-                        console.log('Copying document', documentID, $modal);
+                        copyDialog = this.getCopyDocumentDialog();
+                        copyDialog.sourceDocumentID = documentID;
+                        copyDialog.setValue(documentID + '_copy');
+                        copyDialog.$el.modal('show');
                     } else {
+                        this.destroyDocument(documentID);
                         console.log('Removing document', documentID);
                     }
                 }
                 return false;
+            },
+
+            copyDocumentHandler: function () {
+                var documentsCollection = this.model.value,
+                    copyDocumentDialog = this.getCopyDocumentDialog(),
+                    sourceDocumentID = copyDocumentDialog.sourceDocumentID,
+                    copyDocumentID = copyDocumentDialog.model.value,
+                    document = documentsCollection.get(sourceDocumentID),
+                    documentCopy = document.clone();
+                documentCopy.set('_id', copyDocumentID);
+                documentsCollection.add(documentCopy);
+                documentCopy.save();
+                copyDocumentDialog.modal('hide');
+                this.refreshGrid();
+            },
+
+            cancelDocumentHandler: function () {
+                this.getCopyDocumentDialog().$el.modal('hide');
+            },
+
+            destroyDocument: function (documentID) {
+                var documentsCollection = this.model.value,
+                    document = documentsCollection.get(documentID);
+
+                if (!_.isUndefined(document)) {
+                    document.destroy();
+                    this.refreshGrid();
+                }
             }
         });
 });
